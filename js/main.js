a1335cf22b27540cf5e9d590e986344a3a95a076
Aldo Esteban Vera Zúñiga <aestebanveraz@gmail.com>
2023-11-19 20:31:36 -0300

    exercise 1

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/* 1: Vincular un archivo "index.html" con uno de JS "js/main.js". En el title y h1 principal del archivo HTML incorporar el texto "Ejercicio". 
+Con JS leer directamente el contenido del elemento <title> e imprimirlo en la consola. */
+
+// Acceder al title de html leerlo y mostrarlo por consola
+console.log(document.title);
